0d3188a58fdaf15970319f58b01160f0c2fa7f9d
David Stewart <davidstewart@gmail.com>
2019-05-26 23:21:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,7 @@ def save
     binding.pry
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 
-
+end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
